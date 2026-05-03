05e5607a54f5d28675c04d96bd25d5c02ce94a65
Rupak <118914904+dev-rupak@users.noreply.github.com>
2026-05-03 22:21:27 +0530

    revised pattern problems

--- Java/Practice/Revision.java
+++ Java/Practice/Revision.java
@@ -4,8 +4,121 @@ public class Revision {
     public static void main(String args[]) {
         Revision rev = new Revision();
 
-        System.out.println(rev.reverseDigit(123));
+        rev.hollowRhombus(5);
 
+    }
+
+    void hollowRhombus(int num) {
+        for (int row = num; row >= 1; row--) {
+            for (int col = 1; col < row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= num; col++) {
+                if (row == num || row == 1 || col == 1 || col == num) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void solidRhombus(int num) {
+        for (int row = num; row >= 1; row--) {
+            for (int col = 1; col < row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= num; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    void butterflyPattern(int num) {
+        for (int row = 1; row <= num / 2; row++) {
+            for (int col = 1; col <= num; col++) {
+                if (col <= row || col > (num - row)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int row = num / 2; row < num; row++) {
+            for (int col = 1; col <= num; col++) {
+                if (col <= (num - row) || col > (num - (num - row))) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void zeroOneTriangle(int num) {
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
+                if ((row + col) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void floydTriangle(int num) {
+        int count = 1;
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(count++);
+            }
+            System.out.println();
+        }
+    }
+
+    void invertedHalfPyramidNumber(int num) {
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= num; col++) {
+                if (col <= (num - row + 1)) {
+                    System.out.print(col);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void invertedRotatedHalfPyramid(int num) {
+        for (int row = 0; row < num; row++) {
+            for (int col = 0; col < num; col++) {
+                if (col < (num - 1 - row)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void hollowRectanglePattern(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     int reverseDigit(int num) {
