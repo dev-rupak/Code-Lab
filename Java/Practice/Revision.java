f13e410f6c67f34e43a94c07827f8cc451b22576
Rupak <118914904+dev-rupak@users.noreply.github.com>
2026-05-07 17:19:21 +0530

    after some revision in array

--- Java/Practice/Revision.java
+++ Java/Practice/Revision.java
@@ -1,11 +1,190 @@
 package Java.Practice;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Revision {
     public static void main(String args[]) {
         Revision rev = new Revision();
+        Scanner sc = new Scanner(System.in);
 
-        rev.hollowRhombus(5);
+        int arr[] = new int[5];
+        System.out.printf("Enter %d number : ", arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        // System.out.print("Enter key : ");
+        // int key = sc.nextInt();
+        // System.out.println(rev.binarySearch(arr, key));
+        rev.kadenAlgo(arr);
+        // System.out.println("reverse number is : " + Arrays.toString(arr));
+
+        sc.close();
+    }
+
+    void kadenAlgo(int arr[]) {
+        // TC = O(n), SC = O(1) - only maximum
+
+        int max = Integer.MIN_VALUE;
+        int cs = 0;
+        for (int i : arr) {
+            cs += i;
+            if (cs < 0) {
+                cs = 0;
+            } else {
+                max = Math.max(max, cs);
+            }
+        }
+        System.out.println("max subarray sum : " + max);
+    }
+
+    void prefixLogicSubarraySum(int arr[]) {
+        // TC = O(n2), SC = O(1)
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum < min) {
+                    min = sum;
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        System.out.println("min subarray sum : " + min);
+        System.out.println("max subarray sum : " + max);
+    }
+
+    void prefixSumArray(int arr[]) {
+        // TC = O(n2), SC = O(n)
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < prefix.length; i++) {
+            int sum = 0;
+            for (int j = i; j < prefix.length; j++) {
+                sum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (sum < min) {
+                    min = sum;
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        System.out.println("min subarray sum : " + min);
+        System.out.println("max subarray sum : " + max);
+    }
+
+    void printSubArraySum(int arr[]) {
+        // TC = O(n3), SC = O(1)
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                if (sum < min) {
+                    min = sum;
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        System.out.println("min subarray sum : " + min);
+        System.out.println("max subarray sum : " + max);
+    }
+
+    void printSubArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                System.out.print("(");
+                for (int k = i; k <= j; k++) {
+                    if (j == i || k == j) {
+                        System.out.print(arr[k]);
+                    } else {
+                        System.out.print(arr[k] + ", ");
+                    }
+                }
+                System.out.print(")\t");
+            }
+            System.out.println();
+        }
+    }
+
+    void pairArrayPrint(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                return;
+            }
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + arr[i] + ", " + arr[j] + "), ");
+            }
+            System.out.println();
+        }
+    }
+
+    void reverseArray(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    int binarySearch(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    int largestInArray(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    int linearSearch(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     void hollowRhombus(int num) {
