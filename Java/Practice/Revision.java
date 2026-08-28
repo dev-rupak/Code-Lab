5eb4541da719ae7f972b255bcfebe89f68639b86
Rupak <118914904+dev-rupak@users.noreply.github.com>
2026-08-28 16:10:30 +0530

    correction

--- Java/Practice/Revision.java
+++ Java/Practice/Revision.java
@@ -642,8 +642,8 @@ public class Revision {
 
     void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] >= arr[j + 1]) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
